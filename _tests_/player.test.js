24716cf2da9250be672735255c9e67f90bf9ead8
Jonathan <eimerarts@gmail.com>
2020-07-29 21:58:58 -0500

    lesson 3

--- _tests_/player.test.js
+++ _tests_/player.test.js
@@ -52,4 +52,16 @@ test("gets player's stats as an object", () => {
   
     expect(player.isAlive()).toBeFalsy();
   });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
   
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
